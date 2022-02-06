67e45b24f1a4520a52846de5fc82a0a282d3a0d5
Robin Hanjun Kwon <khj02021@gmail.com>
2022-02-06 19:50:44 +0900

    Apply postsRoutes to index.js

--- index.js
+++ index.js
@@ -4,12 +4,16 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import postsRoutes from './routes/posts.js';
+
 const app = express();
 
 // Apply middleware
 app.use(cors());
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/posts', postsRoutes);
 
 // Initialise environmental constant
 const CONNECTION_URI = process.env.CONNECTION_URI;
